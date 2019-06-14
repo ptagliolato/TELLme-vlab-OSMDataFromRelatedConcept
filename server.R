45d2768db507f751106437bd2eb3000a644f4d18
ptagliolato <ptagliolato@users.noreply.github.com>
2019-06-14 18:06:34 +0200

    initial commit

---
+++ server.R
@@ -0,0 +1,119 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+# Author: Paolo Tagliolato (CNR IREA), Alessandro Oggioni (CNR IREA), Iacopo Neri (Polimi)
+#
+
+library(shiny)
+library(osmdata)
+library(sf)
+library(sp)
+library(leaflet)
+library(leaflet.extras)
+library(mapview)
+
+
+shinyServer(function(input, output, session) {
+
+  # output$feature<-renderUI({
+  #   choices_osm_features<-available_features()
+  #   selectizeInput("features","available features",choices=choices_osm_features)
+  # })
+  # 
+  
+  output$tags<-renderUI({
+    #inutile<-input$features
+    #choices_osmTags<-list("primaria" = "primary", "secondary" ="secondary") 
+    choices_osmTags<-available_tags(input$features)
+    selectizeInput("tags","available tags",choices=choices_osmTags,multiple=TRUE)
+  })
+  
+  output$mapleaflet <- renderLeaflet({
+    leaflet() %>% addTiles() %>%
+      #addProviderTiles(providers$CartoDB.Positron) %>%
+      setView(0,0,1) %>%  addSearchOSM() %>%
+      addDrawToolbar(
+        targetGroup = "draw",
+        polylineOptions=FALSE,
+        markerOptions = FALSE,
+        circleOptions = FALSE,
+        circleMarkerOptions=FALSE,
+        polygonOptions= FALSE,
+        rectangleOptions = drawRectangleOptions(shapeOptions=drawShapeOptions(fillOpacity = 0,color = 'white',weight = 3)),
+        singleFeature=TRUE
+    )
+  })
+  
+  # m<-mapview()
+  # output$mapleaflet <- renderLeaflet({
+  #   m@map %>% setView(0,0,1) %>%  addSearchOSM() %>%
+  #     addDrawToolbar(
+  #       targetGroup = "draw",
+  #       polylineOptions=FALSE,
+  #       markerOptions = FALSE,
+  #       circleOptions = FALSE,
+  #       circleMarkerOptions=FALSE,
+  #       polygonOptions= FALSE,
+  #       rectangleOptions = drawRectangleOptions(shapeOptions=drawShapeOptions(fillOpacity = 0,color = 'white',weight = 3)),
+  #       singleFeature=TRUE
+  #       
+  #     )
+  # })
+     
+  
+  # intercept new bbx
+  observeEvent(input$mapleaflet_draw_new_feature,{
+    
+      proxy<-leafletProxy("mapleaflet")
+      
+      shape<-input$mapleaflet_draw_new_feature
+      
+      polygon_coordinates <- shape$geometry$coordinates
+      
+      feature_type <- shape$properties$feature_type
+      if(feature_type == "rectangle"){
+        NW=polygon_coordinates[[1]][[2]]
+        SE=polygon_coordinates[[1]][[4]]
+        updateTextInput(session,"bbxN",value=paste(NW[[2]]))
+        updateTextInput(session,"bbxW",value=paste(NW[[1]]))
+        updateTextInput(session,"bbxS",value=paste(SE[[2]]))
+        updateTextInput(session,"bbxE",value=paste(SE[[1]]))
+      }
+  })
+  
+  RV<-reactiveValues(polys=NULL,lines=NULL,points=NULL)
+  currentOSMData<-observeEvent(input$downloadOverpass, ignoreInit = TRUE, {
+    
+    #req(input$bbxN, input$bbxE, input$bbxS, input$bbxW)
+    #input$downloadOverpass
+    
+    xmin<-input$bbxW
+    ymin<-input$bbxS
+    xmax<-input$bbxE
+    ymax<-input$bbxN
+    
+    browser()
+    bb<-c(xmin,ymin,xmax,ymax)
+    x <-
+      osmdata::opq(bbox = as.numeric(bb)) %>% # Chiswick Eyot in London, U.K.
+      add_osm_feature(key = 'highway', value = "primary") %>%
+      add_osm_feature(key = 'highway', value = "secondary") %>%
+      add_osm_feature(key = 'highway', value = "tertiary") %>%
+      osmdata_sf()
+    
+    # workaround for leaflet bug when geometry column is named
+    names(st_geometry(x$osm_lines))<-NULL
+    names(st_geometry(x$osm_points))<-NULL
+    names(st_geometry(x$osm_polygons))<-NULL
+    
+    RV$lines<-x$osm_lines
+    RV$points<-x$osm_points
+    RV$polys<-x$osm_polygons
+    
+    proxy<-leafletProxy("mapleaflet")
+    proxy %>% addPolygons(x$osm_lines)
+  })
+    
+})
---
+++ ui.R
@@ -0,0 +1,154 @@
+#
+# TELLme Erasmus Plus Project
+# 
+# Shiny app for ...
+#
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+# cf. https://www.gdal.org/gdaldem.html
+library(shiny)
+# devtools::install_github("RinteRface/shinydashboardPlus", force = TRUE)
+library(shinydashboardPlus)
+library(shinydashboard)
+#devtools::install_github('andrewsali/shinycssloaders')
+library(shinycssloaders)
+#install.packages("shinyjs")
+library(shinyjs)
+library(leaflet)
+library(leaflet.extras)
+library(mapview)
+library(mapedit)
+
+choices_relatedConcepts <- list("Gray infrastructure - roads" = "roads")
+choices_scales <- list("XL", "L", "M")
+# Hp is to obtain these from a reactive value bound to osm available_features
+#choices_osmFeatures<-list("highway" = "highway", "streams" ="streams") 
+choices_osmFeatures<-available_features()
+
+
+#choices_osmTags<-NULL
+
+dashboardPagePlus(
+  skin = "black-light",
+  collapse_sidebar = FALSE,
+  sidebar_fullCollapse=TRUE,
+  dashboardHeaderPlus(
+    title = tagList(
+      tags$div(class = "logo-lg",
+               tags$img(src = "http://tellmehub.get-it.it/static/img/logo1_200px.png", width = "80px", height = "40px"),
+               tags$span("TELLme Erasmus+ Project - TELLme related concepts to OSM data to shapefiles")
+      )
+    ),
+    titleWidth = 400
+    # fixed = FALSE,
+    ,enable_rightsidebar = TRUE,
+    rightSidebarIcon = "gears"
+  ),
+  rightsidebar=rightSidebar(
+    #fileInput("file1", "Choose Digital Elevation Model File (raster image)",
+    #                      multiple = FALSE, accept = c("*/*","*,*",".*")),
+    #            selectInput("mode", "choose mode",choices = choices),
+    # sliderInput("z","Vertical exaggeration",min = 1,max = 50,value = 1),
+    # sliderInput("az","Azimuth of the light",min = 1,max = 360,value = 315),
+    # sliderInput("alt","Altitude of the light",min = 0,max = 90,value = 45)
+    
+  ),
+  sidebar=dashboardSidebar(
+    collapsed = FALSE,
+    disable = FALSE,
+    width = 0,
+    sidebarMenu(
+      menuItem("Elaboration", tabName = "site", icon = icon("map", lib = "font-awesome"))
+      #menuItem("Map", tabName = "map", icon = icon("map", lib = "font-awesome"))
+      # ,
+      # dropdownBlock(
+      #   fileInput("file1", "Choose Digital Elevation Model File (raster image)",
+      #             multiple = FALSE, accept = c("*/*","*,*",".*")),
+      #   selectInput("mode", "choose mode",choices = choices),
+      #   sliderInput("z","Vertical exaggeration",min = 1,max = 50,value = 1),
+      #   sliderInput("az","Azimuth of the light",min = 1,max = 360,value = 315),
+      #   sliderInput("alt","Altitude of the light",min = 0,max = 90,value = 45),
+      #   id="ss",icon = icon("map", lib = "font-awesome"),title="inputs"
+      # )
+    )
+  ),
+  
+  body=dashboardBody(
+    useShinyjs(),
+    
+    tabItems(
+      tabItem(
+        tabName = "site",
+        fluidRow(
+          
+          boxPlus(# inputs menu
+            width=12,
+            #title="input",
+            background = "light-blue",
+            closable=FALSE,status = "primary", solidHeader = FALSE, collapsible = FALSE,
+            #enable_sidebar = FALSE,
+            #style = "background-color:black; color:white; padding: 0 10px;",
+            fluidRow(
+              column(
+                width=3,
+                # fileInput("file1", "Choose Digital Elevation Model File (raster image)",
+                #           multiple = FALSE, accept = c("*/*","*,*",".*"))
+                selectInput("relatedconcept", "choose related concept", choices=choices_relatedConcepts)
+              ),
+              column(width=3,
+                     selectizeInput("features","available features",choices=choices_osmFeatures,multiple=FALSE)
+                     ),
+              column(width=3,
+                     uiOutput("tags")
+                     #selectizeInput("tags","available tags",choices=choices_osmTags,multiple=TRUE)
+              ),
+              column(
+                width=3,
+                selectInput("scale", "choose scale",choices = choices_scales)
+              )
+            ),
+            fluidRow(
+              column(
+                width=5,
+                style="text-align:left",
+                actionButton("downloadOverpass","obtain data for Related Concept")
+              ),
+              column(
+                offset=1,
+                width=3,
+                style="text-align:left;",
+                disabled(actionButton("doPlotMap","Compute and plot output map",icon("cog")))
+              ),
+              column(
+                width=3,
+                style="text-align:right;",
+                disabled(downloadButton("downloadShapeFile", "Download result"))
+              )
+            )
+          ),
+          
+          boxPlus(
+            width=12,
+            leafletOutput("mapleaflet")
+          ),
+          
+          boxPlus(
+            width=12,
+            fluidRow(
+              #column(width=12,textInput("debug","debug")),
+                column(width=3,style="text-align:left", textInput("bbxN", "NW-lat")),
+                column(width=3,style="text-align:left",textInput("bbxW", "NW-lon")),
+                column(width=3,style="text-align:left",textInput("bbxS", "SE-lat")),
+                column(width=3,style="text-align:left",textInput("bbxE","SE-lon"))
+            )
+          )
+        
+          
+        )#end fluidRow
+      )#end tabItem
+    )#end tabItems
+  )#end dashboardBody
+)#end dashboardPagePlus
