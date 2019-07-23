7e26fbb3f4bfca2af18261cbfc29ba52378b54a3
ptagliolato <ptagliolato@users.noreply.github.com>
2019-07-23 11:40:18 +0200

    Fix layout

--- ui.R
+++ ui.R
@@ -23,23 +23,40 @@ library(leaflet.extras)
 #library(mapedit)
 library(osmdata)
 
-choices_relatedConcepts <- list("Gray infrastructure - roads" = "roads")
-choices_scales <- list("XL", "L", "M")
+#choices_relatedConcepts <- list("Gray infrastructure - roads" = "roads")
+#choices_scales <- list("XL", "L", "M")
 # Hp is to obtain these from a reactive value bound to osm available_features
 # choices_osmFeatures <- list("highway" = "highway", "streams" = "streams") 
 #choices_osmFeatures <- osmdata::available_features()
 
-
-
-panels.about<-div(style="padding:1em;",
-                    shiny::helpText(
-                      h4("Description"),p("This application exploits the third-party service overpass API to download OSM data in form of shapefiles (within zip archive)."),
-                      h4("Usage"), p("Select a bounding box, then one or more TELLme related concepts.",
-                                     "Click \"Obtain data for related concept\" and evaluate the retrieved geographical features.",
-                                     "When you are satisfied with the selection, click the \"Download results\" button to obtain a zip archive with a shapefile per related concept." )
-                      ,h4("Credits"), p("Application developed for the TELLme ERASMUS+ project, O4. See"),a("source code repository on github",href="https://github.com/ptagliolato/TELLme-vlab-OSMDataFromRelatedConcept")
+panels.about_text<- shiny::helpText(
+                      p("Download OSM data for a selection of terms from the TELLme Glossary, exploiting the third-party service overpass API"),
+                      h4("Usage"), 
+                      div(style="font-size:smaller;",
+                        p("1. Select one or more TELLme related concepts in the top bar."),
+                        #div(class="leaflet-draw-toolbar leaflet-bar leaflet-draw-toolbar-top",
+                        #                a(style="float:left; margin-right:2px;", class="leaflet-draw-draw-rectangle")),
+                        p("2. Click on the black-square-icon control in the map and draw a rectangle to select a bounding box."),
+                        p("3. Click \"Obtain data for related concept\" and evaluate the retrieved geographical features.",
+                                       "When you are satisfied with the selection,"),
+                        p("4. Click the \"Download results\" button to obtain a zip archive with as many shapefiles as the related concepts." )
+                        ,h4("Credits"), p("Application developed for the TELLme ERASMUS+ project, O4. See"),a("source code repository on github",href="https://github.com/ptagliolato/TELLme-vlab-OSMDataFromRelatedConcept")
+                      )
                     )
-              )
+              
+
+panels.troubleshooting_text<-shiny::helpText(p(style="font-size:smaller;",
+                                               "If you incur in any issue, please check the \"log\" box here provided.",
+                                               "The most common problems are due to the remote overpass service.",
+                                               "You can try to extend the timeout (the time while the application waits for the remote service to answer with data) with the following numeric control.",
+                                               "Note that the bigger the bounding box you select, the longer the time the application needs to wait the results for.",
+                                               "Note also that not all the requests can be handled by the remote service (e.g. too big bounding boxes).",
+                                               "If, after having extended the timeout, you cannot see the expected results,", 
+                                               "you can also copy-paste the query composed by this application from the \"overpass generated query\" box",
+                                               "and you can use it to directly invoke the remote API, via the",
+                                               #"<a href=\"https://overpass-turbo.eu/\">overpass-turbo</a>",
+                                               a("overpass-turbo",href="https://overpass-turbo.eu"),
+                                               "web application, or via a terminal using CURL program."))
 # choices_osmTags <- NULL
 
 dashboardPagePlus(
@@ -59,63 +76,28 @@ dashboardPagePlus(
     rightSidebarIcon = "gears"
   ),
   rightsidebar = rightSidebar( 
-    rightSidebarTabContent(id="help", icon="info", title="About", active = TRUE, panels.about),
-    rightSidebarTabContent(id="controls",icon="gear",title = "Settings",
-      numericInput(inputId="overpass_timeout",label = "overpass timeout", value = "30",min = 10, max=600, step = 10),
-      textAreaInput(inputId="overpass_query", label="overpass generated query", resize="vertical", height = 300)
-    ),
+    rightSidebarTabContent(id="help", icon="info", title="About", active = TRUE, panels.about_text),
     rightSidebarTabContent(id="troubleshooting", icon="fire-extinguisher", title="Troubleshooting",
       textAreaInput(inputId="errorlog", label="reported issues", resize="vertical"),
-      shiny::helpText(p(style="font-size:smaller;",
-        "If you incur in any issue, please check the \"log\" box here provided.",
-        "The most common problems are due to the remote overpass service.",
-        "You can try to extend the timeout (the time while the application waits for the remote service to answer with data) with the following numeric control.",
-        "Note that the bigger the bounding box you select, the longer the time the application needs to wait the results for.",
-        "Note also that not all the requests can be handled by the remote service (e.g. too big bounding boxes).",
-        "If, after having extended the timeout, you cannot see the expected results,", 
-        "you can also copy-paste the query composed by this application from the \"overpass generated query\" box",
-        "and you can use it to directly invoke the remote API, via the",
-        #"<a href=\"https://overpass-turbo.eu/\">overpass-turbo</a>",
-        a("overpass-turbo",href="https://overpass-turbo.eu"),
-        "web application, or via a terminal using CURL program."))
+      panels.troubleshooting_text
+    ),
+    rightSidebarTabContent(id="controls",icon="gear",title = "Settings",
+                           numericInput(inputId="overpass_timeout",label = "overpass timeout", value = "30",min = 10, max=600, step = 10),
+                           div(style="font-size:smaller;",textAreaInput(inputId="overpass_query", label="overpass generated query", resize="vertical", height = 300))
     )
   ),
   sidebar = 
-    # dashboardSidebar(
-    #   disable = FALSE,collapsed = FALSE,
-    #   sidebarMenu(menuItem("Map", tabname="site"))
-    #   #div(style="padding:1em;",
-    #       # shiny::helpText(
-    #       #   h4("Description"),p("This application exploits the third-party service overpass API to download OSM data in form of shapefiles (within zip archive)."),
-    #       #   h4("Usage"), p("Select a bounding box, then one or more TELLme related concepts.", 
-    #       #                  "Click \"Obtain data for related concept\" and evaluate the retrieved geographical features.", 
-    #       #                  "When you are satisfied with the selection, click the \"Download results\" button to obtain a zip archive with a shapefile per related concept." )
-    #       #   ,h4("Credits"), p("Application developed for the TELLme ERASMUS+ project, O4. See"),a("source code repository on github",href="https://github.com/ptagliolato/TELLme-vlab-OSMDataFromRelatedConcept")
-    #       # )
-    #   #)
-    #),
     dashboardSidebar(
       collapsed = TRUE,
       disable = TRUE,
       width = 0,
       sidebarMenu(
         menuItem("Elaboration", tabName = "site", icon = icon("map", lib = "font-awesome"))
-        #menuItem("About", tabName = "about", icon = icon("info", lib = "font-awesome"))
-        #menuItem("Map", tabName = "map", icon = icon("map", lib = "font-awesome"))
-        # ,
-        # dropdownBlock(
-        #   fileInput("file1", "Choose Digital Elevation Model File (raster image)",
-        #             multiple = FALSE, accept = c("*/*","*,*",".*")),
-        #   selectInput("mode", "choose mode", choices = choices),
-        #   sliderInput("z", "Vertical exaggeration", min = 1, max = 50, value = 1),
-        #   sliderInput("az", "Azimuth of the light", min = 1, max = 360, value = 315),
-        #   sliderInput("alt", "Altitude of the light", min = 0, max = 90, value = 45),
-        #   id = "ss", icon = icon("map", lib = "font-awesome"), title = "inputs"
-        # )
       )
   ),
   
   body = dashboardBody(
+    tags$head(tags$style(HTML('#overpass_query, #errorlog{font-size:smaller}'))),
     useShinyjs(),
     tabItems(
       tabItem(
@@ -132,8 +114,6 @@ dashboardPagePlus(
               column(
                 width = 11,
                 offset=1,
-                # fileInput("file1", "Choose Digital Elevation Model File (raster image)",
-                #           multiple = FALSE, accept = c("*/*","*,*",".*"))
                 #selectInput("relatedconcept", "choose related concept", choices = choices_relatedConcepts)
                 uiOutput("relConc")
                )
@@ -175,12 +155,12 @@ dashboardPagePlus(
           boxPlus(
             width = 12,
             fluidRow(
-              column(width=12, textOutput("bbx")),
               #column(width = 12,textInput("debug","debug")),
-                column(width = 3, style = "text-align:left", textInput("bbxN", "NW-lat")),
-                column(width = 3, style = "text-align:left", textInput("bbxW", "NW-lon")),
-                column(width = 3, style = "text-align:left", textInput("bbxS", "SE-lat")),
-                column(width = 3, style = "text-align:left", textInput("bbxE","SE-lon"))
+                column(width = 3, style = "text-align:left", disabled(textInput("bbxN", "NW-lat"))),
+                column(width = 3, style = "text-align:left", disabled(textInput("bbxW", "NW-lon"))),
+                column(width = 3, style = "text-align:left", disabled(textInput("bbxS", "SE-lat"))),
+                column(width = 3, style = "text-align:left", disabled(textInput("bbxE","SE-lon"))),
+                column(width=12, div(style="visibility:hidden",textOutput("bbx")))
             )
             
           )
